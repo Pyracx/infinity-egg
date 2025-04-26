fe33b3f56ad14f4ca4542237f7a8d25f3817dd2e
DxtrPog <gloriouseeee@gmail.com>
2025-04-26 21:49:23 +1000

    Making new test thing

---
+++ AllEggs/AllEggsInstant.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+petlist = {
+    "Emerald Golem": 0.5,
+    "Inferno Dragon": 0.25,
+    "Unicorn": 0.25,
+    "Flying Pig": 0.1,
+    "Lunar Serpant": 0.25,
+    "Electra": 0.1,
+    "Dark Phoenix": 0.05,
+    "Neon Elemental": 0.02,
+    "Null Void": 0.00001,
+    "Inferno Cube": 0.025,
+    "Virus": 0.002, 
+    "Green Hydra": 0.004,
+    "Demonic Hydra": 0.001,
+    "Hexarium": 0.001,
+    "Rainbow Shock": 0.0005,
+}
+
+test = st.selectbox("Pet", petlist, None)
