8326411c3830ad920326e168865fd221a3b6d592
DxtrPog <gloriouseeee@gmail.com>
2025-04-26 18:22:53 +1000

    Update InfinityEggLive.py

--- InfinityEggLive.py
+++ InfinityEggLive.py
@@ -156,7 +156,7 @@ if st.button("Run Simulation"):
 
             # Animation (with time delay) in regular mode
             for i in range(eggs_to_open):
-                print(i)
+                test =  st.text(i)
                 egg_placeholders[i].write(f"🪺 Hatching... {current_eggs[i] if i < len(current_eggs) else ''}")
             time.sleep(hatch_speed*0.0025)
             for placeholder in egg_placeholders:
