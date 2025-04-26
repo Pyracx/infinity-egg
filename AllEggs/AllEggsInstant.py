c282dd8e420cf6fbef915a60e32d335d1c3da4ec
DxtrPog <gloriouseeee@gmail.com>
2025-04-26 22:14:28 +1000

    Update AllEggsInstant.py

--- AllEggs/AllEggsInstant.py
+++ AllEggs/AllEggsInstant.py
@@ -19,20 +19,15 @@ petlist = {
 }
 
 chosen_pet = st.selectbox("Pet", petlist, None)
-time_based = st.checkbox("Use Time & Hatch Speed instead of Number of Eggs (Beta)")
-if time_based:
-    time_spent = st.number_input("Time Spent Hatching (Minutes)", min_value=1, max_value=10_000_000, value=60, step=10)
-    hatch_speed = st.number_input("Hatch Speed", min_value=1, max_value=1000, value=160, step=10)
-else:
-    num_hatches = st.number_input("Number of Eggs to Simulate", min_value=1, max_value=10_000_000, value=100_000, step=1000)
-luck_multiplier = st.slider("Luck Multiplier", 1.0, 27.0, 1., 0.1)
+
+
+hatch_speed = st.number_input("Hatch Speed (%)", min_value=1, max_value=1000, value=160, step=10)
+luck_multiplier = st.slider("Luck Multiplier", 1, 2700, 50, 1)
 shiny_chance = st.slider("Shiny Chance (1 in X)", 13, 40, 40)
 mythic_chance = st.slider("Mythic Chance (1 in X)", 12, 100, 100)
-eggs_to_open = st.slider("Number of Eggs to Hatch at Once", 1, 6, 6)
-#skip_animation = st.checkbox("Skip Animation (Instant Results)") Useless for instant?
-if time_based:
-    num_hatches = math.trunc(((time_spent*(0.1375*hatch_speed))*eggs_to_open))
-    str_num_hatches = st.text("Number of Eggs Hatched: " + str(num_hatches))
+eggs_to_open = st.slider("Number of Eggs you Hatch at Once", 1, 6, 6)
+num_hatches = math.trunc(((time_spent*(0.1375*hatch_speed))*eggs_to_open))
+str_num_hatches = st.text("Number of Eggs Hatched: " + str(num_hatches))
 
 if chosen_pet != None:
     col1, col2 = st.columns([0.3, 0.7])
